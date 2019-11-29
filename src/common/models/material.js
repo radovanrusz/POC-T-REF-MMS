fd9abf0950cdc3ded4f938c6c74d70ec9282f91d
Petr Sorad <petr_sorad@cz.ibm.com>
2019-11-29 20:08:55 +0100

    /mms get filter suport

--- src/common/models/material.js
+++ src/common/models/material.js
@@ -1,13 +1,59 @@
 module.exports = function(Material) {
+  Material.findBy = function (kmat,mvm,limit,cb) {
+    console.log('mms invoked with kmat:'+kmat+', mvm:'+mvm+', limit:'+limit+'  and callBack: ' + cb);
+    var whereClause = {};
+    if (kmat && mvm) {
+      andClause = [];
+      andClause.push({"kmat": { "eq": kmat}});
+      andClause.push({"mvm": { "eq": mvm}});
+      whereClause.and = andClause;
+    } else {
+      var clause = {};
+      if (kmat) {
+        whereClause = {"kmat": { "eq": kmat}};
+      }
+      if (mvm) {
+        whereClause = {"mvm": { "eq": mvm}};
+      }
+    }
+    var filter = {};
+    if (limit) {
+      filter.limit = limit;
+    }
+    if (kmat || mvm) {
+      filter.where = whereClause;
+    }
+    // filter: {"limit":3,"where":{"and":[{"kmat":{"eq":"11111"}},{"mvm":{"eq":"001"}}]}}
+
+    console.log('Resulting filter: ' + JSON.stringify(filter));
+    Material.find(filter,cb);
+  };
+
   Material.listAll = function(cb) {
     console.log('listAll invoked with callBack: ' + cb);
     Material.find({}, cb);
   };
 
+  Material.remoteMethod('findBy', {
+    accepts: [
+      {arg: 'kmat', type: 'string'},
+      {arg: 'mvm', type: 'string'},
+      {arg: 'limit', type: 'number'}
+    ],
+    returns: {
+      arg: 'materials',
+      type: 'array' 
+    },
+    http: {
+      path: '/mms',
+      verb: 'get'
+    }
+  });
+
   Material.remoteMethod('listAll', {
     returns: {
       arg: 'materials',
-      type: 'array'
+      type: 'array' 
     },
     http: {
       path: '/listall',
--- src/server/boot/routes.js
+++ src/server/boot/routes.js
@@ -1,12 +1,38 @@
+
 module.exports = function(app) {
   var router = app.loopback.Router();
 
   router.get('/mms', async (req, res) => {
+    const params = req && req.query;
+    const {mvm = '', kmat = '', limit = ''} = params;
+    console.log('findMat', params);
+    console.log('mvm:', mvm, ' kmat:', kmat, ' limit:', limit);
     try {
-      res.status(201).send("Materials list");
+      /*
+         *   const findMat = {
+         *    kmat: { $eq: kmat },
+         *    mvm: { $eq: mvm }
+         * };
+         */
+        const findMat = {};
+        if (kmat) {
+            findMat.kmat = {$eq: kmat};
+        }
+        if (mvm) {
+            findMat.mvm = {$eq: mvm};
+        }
+        let material = '';
+        if (limit) {
+          material = await Material.find(findMat).limit(Number(limit));
+        } else {
+          material = await Material.find(findMat);
+        }
+        res.json(material); 
+
+      //res.status(201).send("Materials list");
     } catch(e) {
-      console.log(e);
-      res.status(400).send(e);
+      console.error('unable to execute find', e);
+      res.status(500).json({error: e});
     }
   });
   app.use(router);
