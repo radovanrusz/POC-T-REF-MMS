0a53c5685066a995cc105c1a6c3b77c9caf80285
Petr Sorad <petr_sorad@cz.ibm.com>
2019-12-06 17:29:33 +0100

    Implemented update orm move scernarios (except moving part of record)

--- src/common/models/material.js
+++ src/common/models/material.js
@@ -36,89 +36,88 @@ module.exports = function(Material) {
     console.log('mms/put invoked with id: '+id+'kmat:'+kmat+', mvm:'+mvm+', hmotnost:'+hmotnost+', mnozstvi:'+mnozstvi);
     // calculate scenario code; TODO: move to separate fuction
     var scenario = sc.calcScenario(id, kmat, mvm, hmotnost, mnozstvi);
-    // TODO: perform validity chks
-    // ...
+    // perform validity chks
+    if (hmotnost && hmotnost<0) {
+      cb(null,{"msg": "Cannot update nor transfre negative hmotnost!"});
+      return;
+    };
+    if (mnozstvi && mnozstvi<0) {
+      cb(null,{"msg": "Cannot update nor transfre negative mnozstvi!"});
+      return;
+    }
+    // ... hmotnost is null or >0; the same for mnozstvi
     // processing scenarios
     switch(scenario) {
       case sc.SC_NOTHING2UPDATE:
           cb(null,{"msg": "Nothing to update!"});
         break;
       case sc.SC_REGISTER_NEW:
-        var newMat = { "kmat": kmat, "mvm": mvm, "hmotnost": hmotnost, "mnozstvi": mnozstvi };
-        console.log('Registering new item ' + JSON.stringify(newMat) + ' ...');
-        Material.create(newMat, function(err,obj) {
-          if (err) {
-            return err;
-          }
-          console.log("Created row with id: " + obj.id);
-          console.log("Sending kafka event ...")
-          kafka.sendEvent(id, kmat, mvm, hmotnost, mnozstvi, obj, cb);
-        });
+        Material.createNew(kmat, mvm, hmotnost, mnozstvi, cb);
         break;
       case sc.SC_UPD_KMAT:
-        console.log('Updating kmat to ' + kmat + ' for id ' + id);
         var updateData = { "kmat": kmat };
         Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_HMOT:
-        console.log('Updating hmotnost to ' + hmotnost + ' for id ' + id);
         var updateData = { "hmotnost": hmotnost };
         Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_KMAT_HMOT:
-          console.log('Updating kmat to ' + kmat + ', hmotnost to ' + hmotnost + ' for id ' + id);
-          var updateData = { "kmat": kmat, "hmotnost": hmotnost };
-          Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        var updateData = { "kmat": kmat, "hmotnost": hmotnost };
+        Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_MNOZ:
-          console.log('Updating mnozstvi to ' + mnozstvi + ' for id ' + id);
-          var updateData = { "mnozstvi": mnozstvi };
-          Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        var updateData = { "mnozstvi": mnozstvi };
+        Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_HMOT_MNOZ:
-          console.log('Updating hmotnost to ' + hmotnost + ', mnozstvi to ' + mnozstvi + ' for id ' + id);
-          var updateData = { "hmotnost": hmotnost, "mnozstvi": mnozstvi };
-          Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        var updateData = { "hmotnost": hmotnost, "mnozstvi": mnozstvi };
+        Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_KMAT_HMOT_MNOZ:
-          console.log('Updating kmat to ' + kmat + ', hmotnost to ' + hmotnost + ', mnozstvi to ' + mnozstvi + ' for id ' + id);
-          var updateData = { "kmat": kmat, "hmotnost": hmotnost, "mnozstvi": mnozstvi };
-          Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        var updateData = { "kmat": kmat, "hmotnost": hmotnost, "mnozstvi": mnozstvi };
+        Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UPD_KMAT_MNOZ:
-          console.log('Updating kmat to ' + kmat + ', mnozstvi to ' + mnozstvi + ' for id ' + id);
-          var updateData = { "kmat": kmat, "mnozstvi": mnozstvi };
-          Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        var updateData = { "kmat": kmat, "mnozstvi": mnozstvi };
+        Material.updateById(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       case sc.SC_UOM:
-        /*
-        get instance
-        chk mvm(provided)=mvm(DB)
-          - true  : update scenario (nothing to do in this particular case) 
-          - false : move scenario - TODO:
-        */
-        // remove after impl. done!
-        cb(null,{"msg": "Invalid (or not supported yet) scenario!"});
+        var updateData = null;
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_KMAT:
+        var updateData = { "kmat": kmat };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_HMOT:
+        var updateData = { "hmotnost": hmotnost };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_KMAT_HMOT:
+        var updateData = { "kmat": kmat, "hmotnost": hmotnost };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_MNOZ:
+        var updateData = { "mnozstvi": mnozstvi };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_KMAT_MNOZ:
+        var updateData = { "kmat": kmat, "mnozstvi": mnozstvi };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_HMOT_MNOZ:
+        var updateData = { "hmotnost": hmotnost, "mnozstvi": mnozstvi };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+        break;
+      case sc.SC_UOM_KMAT_HMOT_MNOZ:
+        var updateData = { "kmat": kmat, "hmotnost": hmotnost, "mnozstvi": mnozstvi };
+        Material.uOM(id, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
         break;
       default:
-        console.log('Invalid (or not supported yet) scenario!');
-        cb(null,{"msg": "Invalid (or not supported yet) scenario!"});
+        console.log('Invalid scenario!');
+        cb(null,{"msg": "Invalid scenario!"});
     }
-    /*
-    if (scenario == sc.SC_MASS_UPDATE) {  // update hmotnost pres vsechny sklady
-      console.log('updating hmotnost of kmat: ' + kmat + ' to: ' + hmotnost);
-      var updatedCnt = 0;
-      Material.updateAll({"kmat": kmat},{"hmotnost": hmotnost},function(err, info){
-        if (err) {
-          return err;
-        }
-        updatedCnt = info.count;
-        console.log('Updated ' + updatedCnt + ' rows.');
-        //send kafka
-        kafka.sendEvent(kmat, mnozstvi, mvmTo, mvmFrom, hmotnost,'Updated ' + updatedCnt + ' rows.',cb);
-      });
-    }
-    */
   };
 
   Material.listAll = function(cb) {
@@ -166,7 +165,22 @@ module.exports = function(Material) {
   });
 
   // helper methods
+  Material.createNew = function (kmat, mvm, hmotnost, mnozstvi, cb) {
+    var newMat = { "kmat": kmat, "mvm": mvm, "hmotnost": hmotnost, "mnozstvi": mnozstvi };
+    console.log('Registering new item ' + JSON.stringify(newMat) + ' ...');
+    Material.create(newMat, function(err,obj) {
+      if (err) {
+        return err;
+      }
+      console.log("Created row with id: " + obj.id);
+      console.log("Sending kafka event ...")
+      kafka.sendEvent(id, kmat, mvm, hmotnost, mnozstvi, obj, cb);
+    });
+  }
+
+
   Material.updateById = function (id, kmat, mvm, hmotnost, mnozstvi,updateData,cb) {
+    console.log('Updating ' + JSON.stringify(updateData) + ' for id ' + id);
     Material.findById(id,{}, function(err, inst) {
       if (err) {
         console.log('Err. findById fired: ' + JSON.stringify(err));
@@ -183,6 +197,92 @@ module.exports = function(Material) {
         console.log('Updated record: ' + JSON.stringify(uinst));
         kafka.sendEvent(id, kmat, mvm, hmotnost, mnozstvi, uinst, cb);
       });
+    });
+  };
+
+  Material.updateInst = function(inst, kmat, mvm, hmotnost, mnozstvi, updateData, cb) {
+    if (updateData) {
+      console.log('Updating ' + JSON.stringify(updateData) + ' for id ' + inst.id);
+      inst.updateAttributes(updateData, function(err, uinst) {
+        if (err) {
+          console.log('Error when updating material instance: ' + JSON.stringify(err));
+          cb(err);
+          return err;
+        }
+        console.log('Updated record: ' + JSON.stringify(uinst));
+        kafka.sendEvent(inst.id, kmat, mvm, hmotnost, mnozstvi, uinst, cb);
+      });
+    } else {
+      console.log('Nothing to update!');
+      cb(null,{"msg": "Nothing to update!"});
+    }
+  };
+
+  Material.uOM = function (id, kmat, mvm, hmotnost, mnozstvi, updateData, cb) {
+    Material.findById(id,{}, function(err, inst) {
+      if (err) {
+        console.log('Err. findById fired: ' + JSON.stringify(err));
+        cb(err);
+        return err;
+      }
+      console.log('Found instance to work with ' + JSON.stringify(inst));
+      if (inst.mvm == mvm) { // update scenario (nothing to do in this particular case) 
+        Material.updateInst(inst, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+      } else {  // move scenario
+        if (hmotnost && mnozstvi) {
+          cb(null,{"msg": "Cannot transfer by both hmotnost and mnozstvi!"});  
+        } else {
+          if (hmotnost) {  // move by hmotnost
+            if(hmotnost>inst.hmotnost) {
+              cb(null,{"msg": "Hmotnost required to transfer(" + hmotnost + ") is greater than hmotnost present in selected record (" + inst.hmotnost + ")!"});  
+            } else {
+              if (hmotnost == inst.hmotnost) {  // move whole record
+                var updateData = { "mvm": mvm };
+                Material.updateInst(inst, kmat, mvm, hmotnost, mnozstvi, updateData, cb);  
+              } else {  // update existing record and create new one
+                // chk kmat
+                if (kmat && kmat != inst.kmat) {
+                  cb(null,{"msg": "Cannot change kmat during transfer!"});
+                  return;  
+                };
+                var transMnoz = hmotnost * inst.mnozstvi / inst.hmotnost;
+                console.log('Transferring hmotnost: ' + hmotnost + ' (mnozstvi: ' + transMnoz + ') of kmat: ' + inst.kmat + ' from ' + inst.mvm + ' to ' + mvm);
+                // TODO: update and insert
+                /*
+                for update : inst,inst.hmotnost-hmotnost, inst.mnozstvi-transMnoz
+                for insert : inst.kmat, mvn, hmotnost, transMnoz
+                for kafka : id, kmat, mvn, hmotnost, mnozstvi
+                */
+                cb(null,{"msg": "Scenario not implemented yet!"});
+              }
+            }
+          } else  {  // move by mnozstvi
+            if (mnozstvi>inst.mnozstvi) {
+              cb(null,{"msg": "Mnozstvi required to transfer(" + mnozstvi + ") is greater than mnozstvi present in selected record (" + inst.mnozstvi + ")!"});  
+            } else {
+              if (mnozstvi == inst.mnozstvi) {  // move whole record
+                var updateData = { "mvm": mvm };
+                Material.updateInst(inst, kmat, mvm, hmotnost, mnozstvi, updateData, cb);
+              } else {  // update existing record and create new one
+                // chk kmat
+                if (kmat && kmat != inst.kmat) {
+                  cb(null,{"msg": "Cannot change kmat during transfer!"});
+                  return;  
+                };
+                var transHmot = mnozstvi * inst.hmotnost / inst.mnozstvi;
+                console.log('Transferring mnozstvi: ' + mnozstvi + ' (hmotnost: ' + transHmot + ') of kmat: ' + inst.kmat + ' from ' + inst.mvm + ' to ' + mvm);
+                // TODO: update and insert
+                /*
+                for update : inst,inst.hmotnost-transHmot, inst.mnozstvi-mnozstvi
+                for insert : inst.kmat, mvn, transHmot, mnozstvi
+                for kafka : id, kmat, mvn, hmotnost, mnozstvi
+                */
+                cb(null,{"msg": "Scenario not implemented yet!"});
+              }
+            }
+          }
+        }
+      }
     });
   }
 
--- src/common/scenario.js
+++ src/common/scenario.js
@@ -4,15 +4,17 @@ module.exports.SC_UOM = 5;                 // update or move
 module.exports.SC_UOM_KMAT= 7;             // update or move kmat
 module.exports.SC_UPD_HMOT = 9;            // update hmotnost
 module.exports.SC_UPD_KMAT_HMOT = 11;      // update kmat and hmotnost
-
+module.exports.SC_UOM_HMOT = 13;           // update or move hmmot
+module.exports.SC_UOM_KMAT_HMOT = 15;      // update or move kmat, hmotnost
 module.exports.SC_UPD_MNOZ = 17;           // update mnozstvi
 module.exports.SC_UPD_KMAT_MNOZ = 19;      // update kmat, mnozstvi
-
+module.exports.SC_UOM_MNOZ = 21;           // update or move mnozstvi
+module.exports.SC_UOM_KMAT_MNOZ = 23;      // update or move kmat, mnozstvi
 module.exports.SC_UPD_HMOT_MNOZ = 25;      // update hmotnost, mnozstvi
 module.exports.SC_UPD_KMAT_HMOT_MNOZ = 27; // update kmat, hmotnost, mnozstvi
-
+module.exports.SC_UOM_HMOT_MNOZ = 29;      // update or move hmotnost, mnozstvi
 module.exports.SC_REGISTER_NEW = 30;       // insert new record
-
+module.exports.SC_UOM_KMAT_HMOT_MNOZ = 31; // update or move kmat, hmotnost, mnozstvi
 
 //TODO: define constants according valid scenarios
 
